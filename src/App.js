29b0b6647724924bab56854b0e8671254b73a89f
michelleRahman21 <michgomez1807@hotmail.com>
2020-05-26 09:17:08 -0400

    Added linear background to the application

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Toast from 'react-bootstrap/Toast';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+
 import './App.css';
 
-function App() {
+const QueenToast = ({ children, other }) => {
+  const [show, toggleShow] = useState(true);
+
   return (
-    <div className="App">
-    <h1>Queen's Web Template</h1>
-    </div>
+    <>
+      {!show && <Button onClick={() => toggleShow(true)}>About us</Button>}
+      <Toast show={show} onClose={() => toggleShow(false)}>
+        <Toast.Header>
+          <strong className="mr-auto">You’re a queen and will be successful so never doubt yourself! We all need a chance so let’s help each other and change the world together!!</strong>
+        </Toast.Header>
+        <Toast.Body>{children}</Toast.Body>
+      </Toast>
+    </>
   );
-}
+};
+
+const App = () => (
+  <Container className="p-3">
+    <Jumbotron className="jumbotron">
+      <h1 className="header">Queen's Web</h1>
+      <QueenToast>
+        <p>Started in 2020. Reshae Alagbada(Founder) wanted to create a company where she can give back. With the help of her co-founder, Safia Hirsi, Queen's Web grew from an ideal to reality.
+        Queen’s Web is a company to help women gain their professional experience in UX/UI or software development. Queen’s Web pairs UX/UI with software engineers together so they can create and have that experience together!</p>
+        <span role="img" aria-label="tada">
+          🕷️
+        </span>
+      </QueenToast>
+    </Jumbotron>
+  </Container>
+);
 
 export default App;
